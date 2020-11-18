2cdd3ccb5a83c104b4d5b83d792ccf9b3b6d90fb
Diogo <diogogyn100@gmail.com>
2020-11-17 23:09:27 -0300

    Teste do cadastro com POstman

--- src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
+++ src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package com.dos.springBootAPIForum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.dos.springBootAPIForum.controller.dto.TopicoDto;
 import com.dos.springBootAPIForum.controller.form.TopicoForm;
@@ -27,7 +30,7 @@ public class TopicosController {
 	public List<TopicoDto> lista(String nomeCurso) {
 		if(nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
-			return TopicoDto.converter(topicos);
+			return TopicoDto.converter(topicos); 
 		}else {
 			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
 			return TopicoDto.converter(topicos);
@@ -35,8 +38,10 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico ));
 	}
 }
