f8420cb54c35e44fd36c625f9a19bd93baf80bb9
Diogo <diogogyn100@gmail.com>
2020-11-18 23:49:26 -0300

    Detalhamento dos topicos

--- src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
+++ src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
@@ -8,12 +8,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.dos.springBootAPIForum.controller.dto.DetalhesDoTopicoDto;
 import com.dos.springBootAPIForum.controller.dto.TopicoDto;
 import com.dos.springBootAPIForum.controller.form.TopicoForm;
 import com.dos.springBootAPIForum.modelo.Topico;
@@ -45,5 +47,12 @@ public class TopicosController {
 		topicoRepository.save(topico);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico ));
+	}
+	
+	@GetMapping("/{id}")
+	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		
+		return new DetalhesDoTopicoDto(topico);
 	}
 }
