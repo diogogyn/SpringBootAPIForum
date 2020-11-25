0719b278c31df9c19ef58d8fde515beeec6523a5
Diogo <diogogyn100@gmail.com>
2020-11-24 22:04:37 -0300

    Implementando primeira paginacao

--- src/main/java/com/dos/springBootAPIForum/controller/dto/TopicoDto.java
+++ src/main/java/com/dos/springBootAPIForum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import com.dos.springBootAPIForum.modelo.Topico;
 
 public class TopicoDto {
@@ -44,9 +46,9 @@ public class TopicoDto {
 		this.dataCriacao = dataCriacao;
 	}
 
-	public static List<TopicoDto> converter(List<Topico> topicos) {
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
 		
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+		return topicos.map(TopicoDto::new);
 	}
 	
 	
--- src/main/java/com/dos/springBootAPIForum/repository/TopicoRepository.java
+++ src/main/java/com/dos/springBootAPIForum/repository/TopicoRepository.java
@@ -2,10 +2,12 @@ package com.dos.springBootAPIForum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dos.springBootAPIForum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-	List<Topico> findByCurso_Nome(String nomeCurso);
+	Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 }
