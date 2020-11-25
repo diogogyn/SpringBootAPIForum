1a9ecfbed6a440e95a49857c126a62b1461f2c63
Diogo <diogogyn100@gmail.com>
2020-11-24 22:30:02 -0300

    Simplificando a ordenação

--- src/main/java/com/dos/springBootAPIForum/SpringBootApiForumApplication.java
+++ src/main/java/com/dos/springBootAPIForum/SpringBootApiForumApplication.java
@@ -2,8 +2,10 @@ package com.dos.springBootAPIForum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class SpringBootApiForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
+++ src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,10 +41,23 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+// Exemlo de paginacao simples
+//	@GetMapping
+//	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtde, @RequestParam String ordenacao) {
+//		Pageable paginacao = PageRequest.of(pagina, qtde, Direction.ASC, ordenacao);
+//		
+//		if(nomeCurso == null) {
+//			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+//			return TopicoDto.converter(topicos); 
+//		}else {
+//			Page<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
+//			return TopicoDto.converter(topicos);
+//		}
+//	}
+	
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtde, @RequestParam String ordenacao) {
-		Pageable paginacao = PageRequest.of(pagina, qtde, Direction.ASC, ordenacao);
-		
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.DESC) Pageable paginacao) {
+				
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 			return TopicoDto.converter(topicos); 
