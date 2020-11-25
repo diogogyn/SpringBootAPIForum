2fcbe8eceb092b9332e3d191f18178e3e9516440
Diogo <diogogyn100@gmail.com>
2020-11-24 22:12:23 -0300

    Adicionando ordenacao

--- src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
+++ src/main/java/com/dos/springBootAPIForum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,8 +41,8 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtde) {
-		Pageable paginacao = PageRequest.of(pagina, qtde);
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int qtde, @RequestParam String ordenacao) {
+		Pageable paginacao = PageRequest.of(pagina, qtde, Direction.ASC, ordenacao);
 		
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
