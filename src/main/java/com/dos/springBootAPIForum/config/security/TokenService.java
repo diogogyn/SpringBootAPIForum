96da92b3cddd7d8d3d6afa4a858444ba8173626a
Diogo <diogogyn100@gmail.com>
2020-12-01 23:45:43 -0300

    Gerando tokens com JWT

---
+++ src/main/java/com/dos/springBootAPIForum/config/security/TokenService.java
@@ -0,0 +1,37 @@
+package com.dos.springBootAPIForum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.dos.springBootAPIForum.modelo.Usuario;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+	
+	@Value("${forum.jwt.secret}")
+	private String secret;
+	
+	public String gerarToken(Authentication authentication) {
+		Usuario logado = (Usuario) authentication.getPrincipal();
+		Date hoje = new Date();
+		Date dataExp = new Date(hoje.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("API do forum")
+				.setSubject(logado.getId().toString())
+				.setIssuedAt(hoje)
+				.setExpiration(dataExp)
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+}
--- src/main/java/com/dos/springBootAPIForum/controller/form/LoginForm.java
+++ src/main/java/com/dos/springBootAPIForum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package com.dos.springBootAPIForum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 	private String email;
 	private String senha;
@@ -14,6 +16,10 @@ public class LoginForm {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public UsernamePasswordAuthenticationToken converter() {
+		
+		return new UsernamePasswordAuthenticationToken(this.email, this.senha);
 	}
 	
 	 
