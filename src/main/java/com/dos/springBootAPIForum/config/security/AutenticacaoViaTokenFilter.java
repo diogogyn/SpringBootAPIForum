710c11fbe9d10601aec39ec483198ae84bf6e788
Diogo <diogogyn100@gmail.com>
2020-12-04 00:00:14 -0300

    Autenticando ocliente via Spring Security

--- src/main/java/com/dos/springBootAPIForum/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/com/dos/springBootAPIForum/config/security/AutenticacaoViaTokenFilter.java
@@ -7,26 +7,43 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.dos.springBootAPIForum.modelo.Usuario;
+import com.dos.springBootAPIForum.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 	
 	private TokenService tokenService;
+	private UsuarioRepository repository;
 	
-	
-	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository repository) {
 		super();
 		this.tokenService = tokenService;
+		this.repository = repository;
 	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		String token = recuperarToken(request);
-		System.out.println(token);
 		boolean valido = tokenService.isTokenValido(token);
-		System.out.println(valido);
+		if(valido) {
+			autenticarCliente(token);
+		}
 		filterChain.doFilter(request, response);
+		
+	}
+
+	private void autenticarCliente(String token) {
+		Long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = repository.findById(idUsuario).get();
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario,  null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
 	}
 
--- src/main/java/com/dos/springBootAPIForum/config/security/SecurityConfigurations.java
+++ src/main/java/com/dos/springBootAPIForum/config/security/SecurityConfigurations.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.dos.springBootAPIForum.repository.UsuarioRepository;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
@@ -23,6 +25,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	private TokenService tokenService;
 	@Autowired
 	private AutenticacaoService autenticacaoService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	@Bean
@@ -46,7 +51,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class)
 		;
 	}
 	
