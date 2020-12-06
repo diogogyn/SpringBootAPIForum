054d22e8e3d51822ef8a9260d7db7a293afb448f
Diogo <diogogyn100@gmail.com>
2020-12-05 23:41:08 -0300

    Documentação de API com Swagger

---
+++ src/main/java/com/dos/springBootAPIForum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,26 @@
+package com.dos.springBootAPIForum.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.dos.springBootAPIForum.modelo.Usuario;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.dos.springBootAPIForum"))
+				.paths(PathSelectors.ant("/**"))
+				.build()
+				.ignoredParameterTypes(Usuario.class);
+		
+	}
+}
